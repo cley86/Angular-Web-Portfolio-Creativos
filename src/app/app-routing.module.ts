99c6ad9b4608cd9e95a4eebf7004ea003031a592
Clay86 <cley.arevalo@gmail.com>
2024-01-21 19:33:55 -0600

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {InicioComponent} from './componentes/inicio/inicio.component';
+import {ProyectosComponent} from './componentes/proyectos/proyectos.component';
+import {HabilidadesComponent} from './componentes/habilidades/habilidades.component';
+import {ContactoComponent} from './componentes/contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'inicio', component: InicioComponent},
+  {path:'proyectos', component: ProyectosComponent},
+  {path:'habilidades', component: HabilidadesComponent},
+  {path:'contacto', component: ContactoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InicioComponent,
+    ProyectosComponent,
+    HabilidadesComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
