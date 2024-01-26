fe2a79291ebc42dce8bed56a4b6af883020e6403
OmarReyesFarje <156132989+OmarReyesFarje@users.noreply.github.com>
2024-01-25 21:30:08 -0600

    Create proyecto.model.ts

---
+++ src/app/componentes/proyectos/proyecto.model.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+
+@Component({
+  selector: 'app-contacto',
+  templateUrl: './contacto.component.html',
+  styleUrl: './contacto.component.css'
+})
+export class ContactoComponent{
+  formularioContacto: FormGroup;
+
+
+  // Aquí puedes inicializar tu formulario
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.formularioContacto = this.formBuilder.group({
+      name:"",
+      addres:"",
+      telefono:"",
+    })
+  }
+
+
+  // Esta función se ejecutará cuando el formulario se envíe
+  formSubmit() {
+    console.log(this.formularioContacto.value)
+  }
+}
